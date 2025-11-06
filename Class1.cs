c5041a8ea99afb1c5395a570893904ae65c546ab
Jonathan Law <jonathan.law@myblueprint.ca>
2025-11-06 17:13:49 -0500

    Minimal sample

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Playwright;
+using TUnit.Playwright;
+
+namespace TUnitTest
+{
+    public class Class1 : PageTest
+    {
+        [Test]
+        public async Task PerformLoginAsync()
+        {
+            await Page.GotoAsync("https://google.com");
+
+            var title = await Page.MainFrame.TitleAsync();
+            await Assert.That(title).IsEqualTo("Google");
+        }
+    }
+}
